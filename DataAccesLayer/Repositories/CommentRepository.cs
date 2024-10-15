c76ac6eecc11fb2dd39caa4e67587a760011a996
Semih Önol <167634874+semihonoll@users.noreply.github.com>
2024-10-15 17:43:06 +0300

    Add project files.

---
+++ DataAccesLayer/Repositories/CommentRepository.cs
@@ -0,0 +1,38 @@
+﻿using DataAccesLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Repositories
+{
+    public class CommentRepository : IGenericDal<Comment>
+    {
+        public void Delete(Comment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Comment entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Comment entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnailImage { get; set; }//Youtubedaki anasayfadaki küçük gösterimler
+        public string BlogImage { get; set; } //Büyük hali
+        public DateTime BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
+
+        public List<Comment> Comments { get; set; }
+    }
+}
